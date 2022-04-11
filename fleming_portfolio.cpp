f3805cf89263cfc48129b8e2bcd142334460fe63
jrfleming <91241750+jrfleming@users.noreply.github.com>
2022-04-11 03:55:49 -0500

    Create fleming_portfolio.cpp

---
+++ fleming_portfolio.cpp
@@ -0,0 +1,12 @@
+// Joseph Fleming   
+ // CSCE 306; Spring 2022
+ // Portfolio Assignment - fleming_portfolio.cpp
+
+ // Code Summary: This source code file is for the portfolio assignment ...with more details provided here. 
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
