a3b3e0eec2ab779764d79a7332ea5bed595c9617
jrfleming <91241750+jrfleming@users.noreply.github.com>
2022-05-05 22:04:23 -0500

    Update Parent_Class.h

--- Parent_Class.h
+++ Parent_Class.h
@@ -1,1 +1,31 @@
+// Joseph Fleming
+// CSCE 306; Spring 2022
+// portfolio project part 5
 
+
+#pragma once
+#include <tuple>
+#include <vector>
+
+enum class Colour {None, White, Black};
+enum class PieceType {None, Pawn, Queen, King};
+
+//Generic piece implementation
+//Pieces.h Implementation: 
+// Implement Parent Class Piece
+// declare piece and define private and public variables
+
+class Piece {
+private:
+	char symbol = ' '; 
+	int value = 0; //the material value of the piece
+	PieceType pieceType = PieceType::None;
+public:
+	Colour colour;
+	int moveCount = 0;
+	Piece(Colour colour);
+
+	virtual char getSymbol();
+	virtual int getValue();
+	virtual PieceType getPieceType();
+};
