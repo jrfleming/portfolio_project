6ab6060377b1fc7fef8641540dd60791285766eb
jrfleming <91241750+jrfleming@users.noreply.github.com>
2022-05-05 23:55:29 -0500

    Update Piece.cpp

--- Piece.cpp
+++ Piece.cpp
@@ -1,1 +1,18 @@
+#include "Pieces.h"
+#include "algorithm"
 
+Piece::Piece(Colour colour) {
+	Piece::colour = colour;
+	Piece::moveCount = 0;}
+
+char Piece::getSymbol() {
+	return Piece::symbol;
+}
+
+int Piece::getValue() {
+	return Piece::value;
+}
+
+PieceType Piece::getPieceType() {
+	return Piece::pieceType;
+}
